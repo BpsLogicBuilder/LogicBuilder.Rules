8f491aa3a502d55d054c6c4707c02e9ab77f3891
BlaiseD <btaylor@bPolicySolutions.com>
2021-02-19 10:20:30 -0500

    Minver sets the assembly version to <Major>.0.0.0.

--- LogicBuilder.Workflow.Activities.Rules/Common/AssemblyRef.cs
+++ LogicBuilder.Workflow.Activities.Rules/Common/AssemblyRef.cs
@@ -7,7 +7,7 @@ namespace LogicBuilder.Workflow.Activities.Common
 {
     public class AssemblyRef
     {
-        public const string ActivitiesAssemblyRef = "LogicBuilder.Workflow.Activities.Rules, Version=1.0.3.0, Culture=neutral, PublicKeyToken=646893bec0268535";
+        public const string ActivitiesAssemblyRef = "LogicBuilder.Workflow.Activities.Rules, Version=1.0.0.0, Culture=neutral, PublicKeyToken=646893bec0268535";
         public const string DialogAssemblyRef = "LogicBuilder.Workflow.Activities.Rules.Design, Version=1.0.3.0, Culture=neutral, PublicKeyToken=646893bec0268535";
         public const string SystemCore = "System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089";
     }
